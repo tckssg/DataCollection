7aeda55199d600119cc1bd9db14538d17568ff5c
tckssg <tckssg@163.com>
2015-03-16 11:26:02 +0800

    Initial Commit

---
+++ DataCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataCollection
+//
+//  Created by ChuanKai on 15/3/16.
+//  Copyright (c) 2015年 ChuanKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataCollection
+//
+//  Created by ChuanKai on 15/3/16.
+//  Copyright (c) 2015年 ChuanKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
